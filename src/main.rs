1fcf19313471d38b36930baf79132cd7f2b69362
Christopher Wells <cwellsny@nycap.rr.com>
2021-01-29 21:12:58 -0800

    Setup app window to use glade file

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,32 @@
 extern crate gio;
+extern crate glib;
 extern crate gtk;
 
 use gio::prelude::*;
 use gtk::prelude::*;
 
+use gtk::{ApplicationWindow, Builder, ComboBox};
+
 use std::env::args;
 
 fn build_ui(application: &gtk::Application) {
-    let window = gtk::ApplicationWindow::new(application);
+    let glade_src = include_str!("../resources/main.glade");
+    let builder = Builder::from_string(glade_src);
 
-    window.set_title("First GTK+ Program");
-    window.set_border_width(10);
-    window.set_position(gtk::WindowPosition::Center);
-    window.set_default_size(350, 70);
+    let window: ApplicationWindow = builder.get_object("main_window").expect("Couldn't get main_window");
+    window.set_application(Some(application));
 
-    let button = gtk::Button::with_label("Click me!");
-
-    window.add(&button);
+    let _set_combo_box: ComboBox = builder.get_object("set_combo_box").unwrap();
 
     window.show_all();
 }
 
 fn main() {
-    let application =
-        gtk::Application::new(Some("com.github.gtk-rs.examples.basic"), Default::default())
-            .expect("Initialization failed...");
+    let application = gtk::Application::new(
+        Some("com.github.excaliburzero.yugioh-card-entry.main"),
+        Default::default(),
+    )
+    .expect("Initialization failed...");
 
     application.connect_activate(|app| {
         build_ui(app);
