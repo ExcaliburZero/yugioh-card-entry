a165f95d8037c3bd73d517eb63fb6110f2da78e2
Christopher Wells <cwellsny@nycap.rr.com>
2021-02-07 11:40:38 -0800

    Move card set handler into function

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate home;
 extern crate yugioh_card_entry;
 
 use std::path::Path;
+use std::sync::{Arc, Mutex};
 
 use gio::prelude::*;
 use gtk::prelude::*;
@@ -15,6 +16,8 @@ use gtk::{ApplicationWindow, Builder, ComboBox, IconView, ListStore};
 use yugioh_card_entry::{APIConfig, CardInfoRequest, ImageCache, YGOProDeckAPI, API};
 
 use std::env::args;
+
+type LockedAPI = Arc<Mutex<YGOProDeckAPI>>;
 
 fn build_ui(application: &gtk::Application) {
     let mut cache_path = home::home_dir().unwrap();
@@ -38,9 +41,11 @@ fn build_ui(application: &gtk::Application) {
     combo_box.set_active(Some(0));
 
     // Get the card sets and add them to the combo box
-    let mut api = YGOProDeckAPI::new(APIConfig::new("https://db.ygoprodeck.com/api/v7"));
+    let api = Arc::new(Mutex::new(YGOProDeckAPI::new(APIConfig::new(
+        "https://db.ygoprodeck.com/api/v7",
+    ))));
 
-    let cardsets = api.get_cardsets().unwrap();
+    let cardsets = api.lock().unwrap().get_cardsets().unwrap();
     for set in cardsets.iter() {
         store.set(&store.append(), &[0], &[&set.set_name]);
     }
@@ -59,45 +64,7 @@ fn build_ui(application: &gtk::Application) {
 
     // Have card viewer update when user chooses a card set
     combo_box.connect_changed(move |cb| {
-        let mut api = YGOProDeckAPI::new(APIConfig::new("https://db.ygoprodeck.com/api/v7"));
-
-        let cardset = combobox_get_active_value(cb);
-        let cards = api
-            .get_cardinfo(&CardInfoRequest {
-                name: None,
-                cardset: Some(cardset),
-            })
-            .unwrap();
-
-        println!("{:?}", cards);
-
-        // Set the cards
-        let card_icon_view: IconView = builder.get_object("card_item_view").unwrap();
-        let cards_store = card_icon_view
-            .get_model()
-            .unwrap()
-            .downcast::<ListStore>()
-            .unwrap();
-
-        cards_store.clear();
-
-        let width = 200;
-        let height = 200;
-        for card in cards.iter() {
-            let image_path = image_cache
-                .get_image(&card.card_images[0].image_url)
-                .unwrap();
-
-            println!("Got image: {}", image_path);
-
-            let image =
-                gdk_pixbuf::Pixbuf::from_file_at_scale(Path::new(&image_path), width, height, true)
-                    .unwrap();
-
-            cards_store.set(&cards_store.append(), &[0, 1], &[&card.name, &image])
-        }
-
-        println!("num cards: {}", cards.len());
+        card_set_combobox_handler(cb, api.clone(), &image_cache, &builder)
     });
 
     window.show_all();
@@ -117,6 +84,53 @@ fn combobox_get_active_value(combo_box: &ComboBox) -> String {
         .get()
         .unwrap()
         .unwrap()
+}
+
+fn card_set_combobox_handler(
+    cb: &ComboBox,
+    api: LockedAPI,
+    image_cache: &ImageCache,
+    builder: &gtk::Builder,
+) {
+    let cardset = combobox_get_active_value(cb);
+    let cards = api
+        .lock()
+        .unwrap()
+        .get_cardinfo(&CardInfoRequest {
+            name: None,
+            cardset: Some(cardset),
+        })
+        .unwrap();
+
+    println!("{:?}", cards);
+
+    // Set the cards
+    let card_icon_view: IconView = builder.get_object("card_item_view").unwrap();
+    let cards_store = card_icon_view
+        .get_model()
+        .unwrap()
+        .downcast::<ListStore>()
+        .unwrap();
+
+    cards_store.clear();
+
+    let width = 200;
+    let height = 200;
+    for card in cards.iter() {
+        let image_path = image_cache
+            .get_image(&card.card_images[0].image_url)
+            .unwrap();
+
+        println!("Got image: {}", image_path);
+
+        let image =
+            gdk_pixbuf::Pixbuf::from_file_at_scale(Path::new(&image_path), width, height, true)
+                .unwrap();
+
+        cards_store.set(&cards_store.append(), &[0, 1], &[&card.name, &image])
+    }
+
+    println!("num cards: {}", cards.len());
 }
 
 fn main() {
